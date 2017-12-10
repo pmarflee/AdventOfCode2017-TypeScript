763d84353392d63416faf5fbd03cec03e5d8f5d5
Paul Marfleet <paul.marfleet@gmail.com>
2017-12-10 18:49:46 +0000

    Keep track of values in all cells

--- ClientApp/components/day3/day3.ts
+++ ClientApp/components/day3/day3.ts
@@ -4,34 +4,21 @@ import { Component } from 'vue-property-decorator';
 @Component
 export default class Day2Component extends Vue {
     input: number = 1;
-
-    mounted() {
-        /*for (var i = 0; i < this.input; i++) {
-
-        }*/
-    }
+    data: any = {}; 
 
     get position(): [number, number] {
-        var x = 0, y = 0, direction = 0, step = 1;
+        var x = 0, y = 0,
+            pos: [number, number] = [x, y],
+            direction = 0, step = 1;
 
-        for (var i = 1, j = 1; i < this.input; i++) {
-            switch (direction) {
-                case 0:
-                    x++;
-                    break;
-                case 1:
-                    y++;
-                    break;
-                case 2:
-                    x--;
-                    break;
-                case 3:
-                    y--;
-                    break;
-            }
-            if (j === step || j === step * 2) {
-                direction = (direction + 1) % 4;
-            }
+        for (var i = 1, j = 1; i <= this.input; i++) {
+            pos = [x, y];
+            this.data[pos.toString()] = this.score(pos);
+            if (direction === 0) x++;
+            else if (direction === 1) y++;
+            else if (direction === 2) x--;
+            else if (direction === 3) y--;
+            if (j === step || j === step * 2) direction = (direction + 1) % 4;
             if (j === step * 2) {
                 step++;
                 j = 1;
@@ -40,12 +27,15 @@ export default class Day2Component extends Vue {
             }
         }
 
-        return [x, y];
+        return pos;
+    }
+
+    score(position: [number, number]): number {
+        return Math.abs(position[0]) + Math.abs(position[1]);
     }
 
     get part1(): number {
-        var [x, y] = this.position;
-        return Math.abs(x) + Math.abs(y);
+        return this.score(this.position);
     }
 
     get part2(): number {
