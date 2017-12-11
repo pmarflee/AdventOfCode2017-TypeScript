944eed01c669b5f46137dba3ffb4c7b6afd8a093
Paul Marfleet <paul.marfleet@gmail.com>
2017-12-11 19:13:24 +0000

    Added solution for Day 4 Part 2

--- ClientApp/components/day4/day4.ts
+++ ClientApp/components/day4/day4.ts
@@ -6,28 +6,38 @@ import Utils from '../../utils';
 export default class Day4Component extends Vue {
     input: string = '';
     part1: number = 0;
+    part2: number = 0;
 
     mounted() {
         fetch('./day4/input.txt')
             .then(response => response.text() as Promise<string>)
             .then(data => {
                 this.input = data;
-                this.part1 = this.calculatePart1();
+                this.part1 = this.countValidPasscodes(this.mustNotContainDuplicates);
+                this.part2 = this.countValidPasscodes(this.mustNotContainAnagrams);
             });
     }
 
-    calculatePart1(): number {
-        return Utils.parseAsWords(this.input, undefined, ' ')
-            .filter(this.isAValidPasscode)
-            .length;
-    }
-
-    isAValidPasscode(line: string[]): boolean {
-        var dict: any = {};
-        for (var word of line) {
+    mustNotContainDuplicates(dict: any): (word: string, dict: any) => boolean {
+        return word => {
             if (dict[word]) return false;
             dict[word] = word;
+            return true;
         }
-        return true;
+    };
+
+    mustNotContainAnagrams(dict: any): (word: string, dict: any) => boolean {
+        return word => {
+            var sorted = [...word].sort().toString();
+            if (dict[sorted]) return false;
+            dict[sorted] = word;
+            return true;
+        }
+    }
+
+    countValidPasscodes(condition: (dict: any) => (word: string, dict: any) => boolean): number {
+        return Utils.parseAsWords(this.input, undefined, ' ')
+            .filter(line => line.every(condition({})))
+            .length;
     }
 }
--- ClientApp/components/day4/day4.vue
+++ ClientApp/components/day4/day4.vue
@@ -2,6 +2,7 @@
     <div>
         <h1>Day 4</h1>
         <h2 v-if="input.length">Part 1: {{ part1 }}</h2>
+        <h2 v-if="input.length">Part 2: {{ part2 }}</h2>
         <p v-else>Loading...</p>
     </div>
 </template>
