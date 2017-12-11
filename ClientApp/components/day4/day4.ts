e6eff9994d785716d107dca92082dda21772a283
Paul Marfleet <paul.marfleet@gmail.com>
2017-12-11 19:27:26 +0000

    Refactored solution for Day 4 to remove duplication of logic

--- ClientApp/components/day4/day4.ts
+++ ClientApp/components/day4/day4.ts
@@ -13,31 +13,23 @@ export default class Day4Component extends Vue {
             .then(response => response.text() as Promise<string>)
             .then(data => {
                 this.input = data;
-                this.part1 = this.countValidPasscodes(this.mustNotContainDuplicates);
-                this.part2 = this.countValidPasscodes(this.mustNotContainAnagrams);
+                this.part1 = this.countValidPasscodes(word => word);
+                this.part2 = this.countValidPasscodes(word => [...word].sort().toString());
             });
     }
 
-    mustNotContainDuplicates(dict: any): (word: string, dict: any) => boolean {
+    mustBeAValidPasscode(dict: any, getKey: (word: string) => string): (word: string, dict: any) => boolean {
         return word => {
-            if (dict[word]) return false;
-            dict[word] = word;
+            var key = getKey(word);
+            if (dict[key]) return false;
+            dict[key] = word;
             return true;
         }
-    };
+    } 
 
-    mustNotContainAnagrams(dict: any): (word: string, dict: any) => boolean {
-        return word => {
-            var sorted = [...word].sort().toString();
-            if (dict[sorted]) return false;
-            dict[sorted] = word;
-            return true;
-        }
-    }
-
-    countValidPasscodes(condition: (dict: any) => (word: string, dict: any) => boolean): number {
+    countValidPasscodes(getKey: (word: string) => string): number {
         return Utils.parseAsWords(this.input, undefined, ' ')
-            .filter(line => line.every(condition({})))
+            .filter(line => line.every(this.mustBeAValidPasscode({}, getKey)))
             .length;
     }
 }
