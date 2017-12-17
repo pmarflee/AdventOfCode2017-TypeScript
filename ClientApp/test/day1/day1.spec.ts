3e8430869cc957d896ac5e61090f3d39df37155d
Paul Marfleet <paul.marfleet@gmail.com>
2017-12-17 22:37:52 +0000

    Added Alsatian module for unit testing Added unit test spec for Day 1

---
+++ ClientApp/test/day1/day1.spec.ts
@@ -0,0 +1,11 @@
+﻿import { AsyncTest, Expect, Test, TestCase, TestFixture } from 'alsatian';
+import Checksum from '../../components/day1/checksum';
+
+@TestFixture('Day 1')
+export class Day1Tests {
+
+    @TestCase('1111', 1, 4)
+    public checksumTest(input: string, part: number, expected: number) {
+        Expect(Checksum.calculate(input, part)).toBe(expected);
+    }
+}
